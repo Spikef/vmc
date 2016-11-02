b53096a4900ef9f9347c8335b95a9e29b9ea7f03
spikef <spikef@foxmail.com>
2016-11-02 16:02:17 +0800

    first commit

---
+++ examples/drivers/routes.js
@@ -0,0 +1,11 @@
+import pages from './pages';
+
+export default {
+    // 首页
+    '/': {
+        name: 'index',
+        doc_title: '首页',
+        component: pages.index,
+        depth: 10
+    }
+};
---
+++ examples/main.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import App from './App';
+import Touch from 'vue-touch';
+import Router from 'vue-router';
+
+Vue.config.debug = true;    // 开启调试模式
+
+var log = console.log;
+console.log = function() {
+    var args = Array.from(arguments);
+    args = args.map(arg => {
+        try{
+            return JSON.parse(JSON.stringify(arg));
+        } catch (e) {
+            return arg;
+        }
+    });
+
+    log.apply(console, args);
+};
+
+import routes from './drivers/routes';
+
+Vue.use(Touch);
+Vue.use(Router);
+
+var router = new Router();
+
+router.map(routes);
+
+router.redirect({
+    '*': '/'
+});
+
+router.beforeEach(t => {
+    if (t.to.doc_title) {
+        window.document.title = t.to.doc_title;
+    }
+
+    t.next();
+});
+
+router.start(App, 'body');
+
+
+// var attachFastClick = require('fastclick');
+// attachFastClick.attach(document.body);
