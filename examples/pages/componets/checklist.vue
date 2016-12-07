ca20349e9eec563917f4084780b1083a45dbb470
zhuhongyi931125 <805948164@qq.com>
2016-12-07 10:43:25 +0800

    update

--- examples/pages/componets/checklist.vue
+++ examples/pages/componets/checklist.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <navbar>checklist</navbar>
-        <div class="content">
-        <div class="alipay" >
+        <div class="cot">
+        <div class="ali" >
             <p>复选框列表</p>
             <checkbox name="pay" id="a"  value="选项A" :checked.sync="aa">选项A</checkbox>
             <checkbox name="pay" id="b" value="选项B" :checked.sync="aa" >选项B</checkbox>
             <checkbox name="pay" id="c" value="选项C" :checked.sync="aa" >选项C</checkbox>
         </div>
-        <div class="sel"> <span class="s">选中的项:</span> <span class="se"> {{ aa | json }} </span></div>
+        <div class="sl"> <span class="s">选中的项:</span> <span class="se"> {{ aa | json }} </span></div>
         </div>
     </div>
 </template>
@@ -34,28 +34,30 @@
 
 </script>
 <style rel="stylesheet/less" lang="less">
-
+    .cot{
+       width: 100%;height: 100%
+    }
     p{
         font-size:15px;
         padding-left:18px;
 
     }
-    .content{
+    .ali{
+        padding-left:10px;
+        height:125px;
         width:100%;
-        height:200px;
-
+        background-color: #fafafa;
     }
-    .alipay{
-        padding-top:20px;
-        padding-left:20px;
-
-    }
-    .sel{
-        padding-left:20px;
-        padding-top:15px;
+    .sl{
+        padding-left:10px;
+        margin-top:20px;
+        width:100%;
+        height:40px;
+        background-color: #fafafa;
     }
     .se{
-        padding-left:30px;
+        padding-left:10px;
     }
+
 
 </style>
--- examples/pages/componets/radio.vue
+++ examples/pages/componets/radio.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <navbar>radio</navbar>
-        <div class="content">
+        <div class="cnt" style="width:100%;height:100%">
         <div class="alipay" >
             <p>单选框列表</p>
-            <radio name="pay" id="alipay" value="选项A" :checked.sync="nn" >选项A</radio>
-            <radio name="pay" id="wechat" value="选项B" :checked.sync="nn">选项B</radio>
-            <radio name="pay" id="wech" value="选项C" :checked.sync="nn">选项C</radio>
+            <radio name="p" id="alipay" value="选项A" :checked.sync="nn" >选项A</radio>
+            <radio name="p" id="wechat" value="选项B" :checked.sync="nn">选项B</radio>
+            <radio name="p" id="wech" value="选项C" :checked.sync="nn">选项C</radio>
         </div>
             <div class="sel"><span>选中的项:</span><span class="se">{{nn}}</span>
             </div>
@@ -30,3 +30,21 @@
 
     }
 </script>
+<style rel="stylesheet/less" lang="less">
+    .alipay{
+        padding-left:10px;
+        height:125px;
+        width:100%;
+        background-color: #fafafa;
+
+    }
+    .sel{
+        padding-left:10px;
+        margin-top:20px;
+        width:100%;
+        height:40px;
+        background-color: #fafafa;
+    }
+
+
+</style>
